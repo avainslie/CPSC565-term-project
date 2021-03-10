2ab1d911b6d23ed1c516870553e11ab49fd491dd
Sammy Elrafih <sammy.elrafih1@ucalgary.ca>
2021-03-09 21:40:30 -0700

    Added basic cell spawner and movement
    You can add how many cells spawn and they have basic movement.

--- Term Project/Assets/Cells/Scripts/CellBehaviour.cs
+++ Term Project/Assets/Cells/Scripts/CellBehaviour.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class CellBehaviour : MonoBehaviour
 {
+    private Rigidbody physicsBody;
+    Vector3 force;
     // Start is called before the first frame update
+
+    public void constructor()
+    {
+
+    }
+
     void Start()
     {
-        
+        physicsBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        force = new Vector3 (Random.Range(-10,10),-0.1f,Random.Range(-10,10));
+        physicsBody.AddForce(force/500);
     }
 }
---
+++ Term Project/Assets/Cells/Scripts/globals.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class globals : MonoBehaviour
+{
+    public static globals Instance { get; private set; }
+    // Start is called before the first frame update
+    //[5]Reso Coder (2017, Mar 24) Singletons in Unity - Simple Tutorial for Beginners - https://www.youtube.com/watch?v=CPKAgyp8cno&ab_channel=ResoCoder
+    public int numberOfCells;
+    private void Awake()
+    {
+        if ( Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else{
+            Destroy(gameObject);
+        }
+    }
+}
